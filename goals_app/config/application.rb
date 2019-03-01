81d3a46e984dec28d080aae3a34a805605f5ab9b
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-01 17:33:31 -0500

    Fail to have good comitting practice

--- goals_app/config/application.rb
+++ goals_app/config/application.rb
@@ -15,5 +15,22 @@ module GoalsApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # config.generators do |g|
+    #   g.test_framework :rspec, #<-- Tells rails to use rspec for testing
+    #     :fixtures => false, # <-- Fixtures are rails default way of creating objects for testing, we will do this ourselves.
+    #     :view_specs => false,
+    #     :helper_specs => false,
+    #     :routing_specs => false,
+    #     :controller_specs => true,
+    #     :request_specs => false
+
+    #     # all of the other falses here are telling rails not to auto generate tests for those types of files.  We will do these manually for now, but as you grow with rails knowledge you may want to switch this to have the files get generated for you.
+    #     # When we talk about auto-generating, I mean when we run the command `rails g model` or  `rails g controller`.  We don't need to run those commands because we already have our models and controllers, but it's good to know.
+    # end
   end
+
+  
 end
+
+
